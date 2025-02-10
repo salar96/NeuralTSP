a995f6bc7ef5a8cf007136dce1c779ef66d74c22
Salar Basiri <50495107+salar96@users.noreply.github.com>
2025-02-10 16:11:57 -0600

    Add files via upload

---
+++ NeuralTSP/LSTM/TSPNet.py
@@ -0,0 +1,116 @@
+import torch 
+import torch.nn as nn
+
+class Encoder(nn.Module):
+    def __init__(self, input_dim, hidden_dim, num_layers):
+        super(Encoder,self).__init__()
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+        self.lstm = nn.LSTM(hidden_dim, hidden_dim, num_layers, batch_first=True, dropout= 0.3)
+        
+    def forward(self, x):
+        y = self.fc1(x)
+        out , _ = self.lstm(y)
+        return out
+
+
+class Decoder(nn.Module):
+    def __init__(self, input_dim, hidden_dim, num_layers, num_heads):
+        super(Decoder,self).__init__()
+        self.lstm = nn.LSTM(input_dim, hidden_dim, num_layers, batch_first= True, dropout= 0.3)
+        self.attn = nn.MultiheadAttention(hidden_dim, num_heads, dropout= 0.3, batch_first= True)
+
+    def forward(self, x, enc_outs, h0, c0, indices_to_ignore):
+        # LSTM output
+        """ x: the vector of the next chosen city, dimension is input_dim
+            enc_outs: output of the encoder used in the attention to generate the probability of visiting the next city
+            h0,c0: first LSTM latent inputs,
+            idices_to_ignor: ignor the visited cities in the attention
+        """
+        y, (hn, cn) = self.lstm(x, (h0, c0))  # y: (N, 1, d)
+
+        # Create a mask for attention
+        # enc_outs: (N, L, d), indices_to_ignore: (N, s)
+        N, L, _ = enc_outs.shape
+        mask = torch.zeros((N, L), dtype=torch.bool, device=enc_outs.device)  # Initialize mask to False
+
+        # Set True for indices to ignore
+        if not indices_to_ignore is None:
+            # for i in range(N):
+            #     mask[i, indices_to_ignore[i]] = True
+            mask[torch.arange(N).unsqueeze(1), indices_to_ignore] = True
+
+        # Apply attention with mask
+        _ , attn_weights = self.attn(query=y, key=enc_outs, value=enc_outs, key_padding_mask=mask)  # Masked attention
+        attn_weights = attn_weights.squeeze(1)  # (N, L)
+
+        return (hn, cn), attn_weights
+
+
+class TSPNet(nn.Module):
+    def __init__(self, input_dim, hidden_dim, device, Num_L_enc =3, 
+                Num_L_dec = 3, num_heads = 2):
+        super(TSPNet, self).__init__()
+        self.Num_L_dec = Num_L_dec
+        self.hidden_dim = hidden_dim
+        self.encoder = Encoder(input_dim, hidden_dim, Num_L_enc).to(device)
+        self.decoder = Decoder(hidden_dim, hidden_dim, Num_L_dec, num_heads).to(device)
+        self.device = device
+    def forward(self, X, mod = 'train'):
+        
+        """ Given an input city data X (batch_size, num_cities, dimension),
+        it returns  outs (batch_size, num_cities+1, num_cities) which is
+        pi(a_t|s_t) the policy: probability of visiting the next city
+        and action_indices (batch_size, num_cities+1, 1) which is the chosen
+        action a_t.
+        """
+
+        batch_size, seq_length, _ = X.size()
+
+        encoded_cities = self.encoder(X) # output shape: (batch_size, num_cities, hidden_dim)
+
+        h0,c0 = torch.zeros(self.Num_L_dec, batch_size, self.hidden_dim).to(self.device), torch.zeros(self.Num_L_dec, batch_size, self.hidden_dim).to(self.device)
+        
+        start_token = nn.Parameter(torch.zeros(1, 1, self.hidden_dim))
+        x_star = start_token.expand(batch_size, -1, -1).to(self.device)# the first input to the decoder is a vector we have to learn
+
+        outs = torch.zeros(batch_size, seq_length+1, seq_length).to(self.device)
+        action_indices = torch.zeros(batch_size, seq_length+1, 1).to(self.device)
+
+        indices_to_ignore = None # for the first input, we can visit all the cities.
+        
+        for t in range(seq_length+1):
+            if t == seq_length:
+                indices_to_ignore = indices_to_ignore[:,1:] # now we can return to the first city we visited
+            (hn,cn), attn_weights = self.decoder(x_star, encoded_cities, h0,c0, indices_to_ignore)
+            attn_weights = torch.clamp(attn_weights, min=1e-9)
+            attn_weights = attn_weights / attn_weights.sum(dim=-1, keepdim=True) # (N, L)
+            if mod == 'train':
+                idx = torch.multinomial(attn_weights, num_samples=1).squeeze(-1) # (N,)
+            elif mod == 'eval_greedy':
+                idx = torch.argmax(attn_weights, dim=-1) # (N,)
+            else:
+                raise('wrong mode')
+            x_star = encoded_cities[torch.arange(batch_size), idx, :].unsqueeze(1) # (N, 1, d)
+            outs[:,t,:] = attn_weights
+            action_indices[:,t,0] = idx
+            h0,c0 = hn,cn
+            if t==0:
+                indices_to_ignore = idx.unsqueeze(-1) # (N,1)
+            else:
+                indices_to_ignore = torch.cat((indices_to_ignore, idx.unsqueeze(-1)),dim=-1).long()
+            
+        return outs, action_indices
+
+
+if __name__ == "__main__":
+    input_dim = 2
+    hidden_dim = 128
+    num_layers = 2
+    num_heads = 1
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    model = TSPNet(input_dim, hidden_dim, device, num_layers, num_layers, num_heads)
+    print(model)
+
+
+
+
---
+++ NeuralTSP/TF/train.py
@@ -0,0 +1,64 @@
+from tqdm import tqdm
+import torch
+from torch import optim
+from utils import *
+from datetime import datetime
+from torch_optimizer import RAdam
+
+
+
+def train(model, preloaded_batches, writer, lr = 0.001, len_print = 100, use_base = True, alpha = 0.9, check_params = False):
+    device = model.device
+    # optimizer = optim.Adam(model.parameters(), lr=lr)
+    optimizer = RAdam(model.parameters(), lr=lr)
+    print(" Training Started with Base and alpha: ",use_base , alpha)
+    batch_means = torch.zeros(len_print)
+    best_mean_cost = float('inf')  # Initialize with infinity
+    best_model_state = None
+    Base = torch.tensor([0.0],device = device)
+    
+    for episode, data_batch in tqdm(enumerate(preloaded_batches)):
+        #data_batch = data_batch.to(device, non_blocking=True)
+        outs, actions = model(data_batch, mod = 'train')
+        
+        sum_log_prob = torch.log(outs.gather(2, actions.long()).squeeze(-1)).sum(dim=1)
+        costs = route_cost(data_batch, actions)
+
+        batch_means[episode % len_print] = costs.mean().item()
+        
+        if use_base is True:
+          policy_loss = torch.sum(sum_log_prob * (costs - Base)) / len(preloaded_batches)
+        else:
+          policy_loss = torch.sum(sum_log_prob * costs) / len(preloaded_batches)
+        
+        optimizer.zero_grad()
+        policy_loss.backward()
+        optimizer.step()
+        if check_params:
+              check_model_weights(model)
+              check_gradients(model)
+        
+        if episode % len_print == len_print - 1:
+            
+            mean_cost = batch_means.mean().item()
+            if alpha == -1:
+              alpha_t = episode/(episode+1)
+              Base = alpha_t * Base + (1-alpha_t) * mean_cost
+              
+            else:
+              Base = alpha * Base + (1-alpha) * mean_cost
+            print(f"Episode: {episode+1} Mean cost: {mean_cost:.2f}")
+            writer.add_scalar('Mean cost', mean_cost, episode)
+            if mean_cost < best_mean_cost:
+                best_mean_cost = mean_cost
+                best_model_state = model.state_dict()
+    if best_model_state is not None:
+        torch.save(best_model_state, 'Saved models/' + str(alpha) + datetime.now().strftime(("%Y_%m_%d %H_%M_%S")) + str(mean_cost) +'best_model.pth')
+        print(f"Best model saved with mean cost: {best_mean_cost:.2f}")
+    writer.close()
+    torch.save(model.state_dict(), 'Saved models/' + str(alpha) + datetime.now().strftime(("%Y_%m_%d %H_%M_%S")) + str(mean_cost) + 'last_model.pth')
+    print("Training Finished")
+    return model
+
+if __name__ == "__main__":
+    print("test")
